588d10944750791305fb42b4ef1a18671b98a90f
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-04-10 17:35:38 +0200

    Added another shade of twitter grey

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         "twitter-blue1-10": "rgba(29, 155, 240, .1)",
         "twitter-blue2": "rgba(26, 140, 216, 1)",
         "twitter-grey1": "rgba(83, 100, 113, 1)",
+        "twitter-grey1-50": "rgba(83, 100, 113, .5)",
         "twitter-grey1-10": "rgba(83, 100, 113, .1)"
       }
     },
