a2ac24c3ca27597ff93a462780148d3ff9bccca2
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-04-11 17:29:34 +0200

    Created the api to create a new tweet

---
+++ tweet_post.py
@@ -0,0 +1,91 @@
+from bottle import post, response, request
+import g
+import time
+from datetime import datetime
+import pymysql
+import json
+
+##############################
+@post("/tweet")
+@post("/<language>/tweet")
+def _(language="en"):
+    if f"{language}_server_error" not in g.ERRORS : language = "en"
+
+    try:
+        # Validation
+        if not request.forms.get("user_id"):
+            errors = {
+                "en_error":"user_id is missing.",
+                "da_error":"user_id mangler."
+            }
+            return g._SEND(400, errors[f"{language}_error"])
+
+        tweet_text, error = g._IS_TWEET_TEXT(request.forms.get("tweet_text"), language)
+        if error: return g._SEND(400, error)
+
+        tweet_image = ""
+        if request.files.get("tweet_image"):
+            tweet_image, error = g._IS_IMAGE(request.files.get("tweet_image"), language)
+            if error: return g._SEND(400, error)
+        
+        tweet_created_at = str(int(time.time()))
+        tweet_created_at_date = datetime.now().strftime("%Y-%B-%d-%A %H:%M:%S")
+        tweet_updated_at = ""
+        tweet_updated_at_date = ""
+        user_id = request.forms.get("user_id")
+
+        tweet = (
+            tweet_text,
+            tweet_image,
+            tweet_created_at,
+            tweet_created_at_date,
+            tweet_updated_at,
+            tweet_updated_at_date,
+            user_id
+        )
+
+    except Exception as ex:
+        print(ex)
+        return g._SEND(500, g.ERRORS[f"{language}_server_error"])
+
+    try:
+        db_connect = pymysql.connect(**g.DB_CONFIG)
+        db = db_connect.cursor()
+
+        query = """
+                INSERT INTO tweets
+                (
+                    tweet_text,
+                    tweet_image,
+                    tweet_created_at,
+                    tweet_created_at_date,
+                    tweet_updated_at,
+                    tweet_updated_at_date,
+                    user_id
+                )
+                VALUES(%s, %s, %s, %s, %s, %s, %s) 
+        """
+        db.execute(query, tweet)
+        db_connect.commit()
+        
+        response.status = 201
+        tweet_id = db.lastrowid
+        tweet = {
+            "tweet_id":tweet_id,
+            "tweet_text":tweet_text,
+            "tweet_image":tweet_image,
+            "tweet_created_at":tweet_created_at,
+            "tweet_created_at_date":tweet_created_at_date,
+            "tweet_updated_at":tweet_updated_at,
+            "tweet_updated_at_date":tweet_updated_at_date,
+            "user_id":user_id
+        }
+
+        return json.dumps(tweet)
+
+    except Exception as ex:
+        print(ex)
+        return g._SEND(500, g.ERRORS[f"{language}_server_error"])
+
+    finally:
+        db.close()
