33c106c0b369abe4368001c5586adf605f88d9b2
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-04-11 11:49:55 +0200

    implemented spa functionality

--- home_get.py
+++ home_get.py
@@ -1,20 +1,10 @@
-from bottle import get, view
-
-##############################
-tabs = [
-  {"icon": "fas fa-home fa-fw", "title": "Home", "id":"home"},
-  {"icon": "fas fa-hashtag fa-fw", "title": "Explore", "id": "explore"},
-  {"icon": "far fa-bell fa-fw", "title": "Notifications", "id": "notifications"},
-  {"icon": "far fa-envelope fa-fw", "title": "Messages", "id": "messages"},
-  {"icon": "far fa-bookmark fa-fw", "title": "Bookmarks", "id": "bookmarks"},
-  {"icon": "fas fa-clipboard-list fa-fw", "title": "Lists", "id": "lists"},
-  {"icon": "far fa-user fa-fw", "title": "Profile", "id": "profile"},
-  {"icon": "fas fa-ellipsis-h fa-fw", "title": "More", "id": "more"}
-]
+from bottle import get, request, view
+import g
 
 ##############################
 @get("/home")
 @get("/<language>/home")
 @view("home")
 def _(language = "en"):
-    return dict(tabs=tabs)
+    is_fetch = True if request.headers.get('From-Fetch') else False
+    return dict(tabs=g.TABS, title="Home", is_fetch=is_fetch)
